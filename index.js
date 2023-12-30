c32a56f9cfc71f3c942ed69eb8684a3d3ca36e4c
macerie <sviluppo+git@sanguineti.net>
2023-12-30 09:51:48 +0100

    ${headersInBody(headers)}

--- index.js
+++ index.js
@@ -252,6 +252,7 @@ const {
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="/plugins/public/sc-vr-theme/bootstrap.bundle.min.js"></script>      
       <script src="/plugins/public/sc-vr-theme/htmx.min.js"></script>      
+      ${headersInBody(headers)}
       <style>
         .form-group {
           margin-bottom: 1rem;
@@ -264,7 +265,6 @@ const {
     </head>
     <body ${bodyAttr} hx-boost="true">
     ${body}
-      ${headersInBody(headers)}
   </body>
   </html>`;
   
